82618fad471830c55113551b8ee156d2ca76f360
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-13 22:12:40 +0700

    Change to Observable

--- app/src/main/java/com/naijab/sitnotify/network/NotifyAPI.kt
+++ app/src/main/java/com/naijab/sitnotify/network/NotifyAPI.kt
@@ -1,13 +1,13 @@
 package com.naijab.sitnotify.network
 
 import com.naijab.sitnotify.newsmenu.model.NewsModel
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.Query
 
 
 interface NotifyAPI {
 
     fun getNews(@Query("key") key: String)
-    : Call<NewsModel>
+    : Observable<NewsModel>
 
 }
