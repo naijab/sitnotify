02b0f35236c92e2a7e4a34dbe0dfb54c38f191f9
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-13 22:15:23 +0700

    Chage to Flowable repleace observable

--- app/src/main/java/com/naijab/sitnotify/network/NotifyAPI.kt
+++ app/src/main/java/com/naijab/sitnotify/network/NotifyAPI.kt
@@ -1,13 +1,13 @@
 package com.naijab.sitnotify.network
 
 import com.naijab.sitnotify.newsmenu.model.NewsModel
-import io.reactivex.Observable
+import io.reactivex.Flowable
 import retrofit2.http.Query
 
 
 interface NotifyAPI {
 
     fun getNews(@Query("key") key: String)
-    : Observable<NewsModel>
+    : Flowable<NewsModel>
 
 }
