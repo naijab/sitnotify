34878fbdfc84c30984cae661c9c2823595e6d87a
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-14 13:44:21 +0700

    Add SerailizedName for variable

--- app/src/main/java/com/naijab/sitnotify/newsmenu/model/NewsModel.kt
+++ app/src/main/java/com/naijab/sitnotify/newsmenu/model/NewsModel.kt
@@ -3,15 +3,18 @@ package com.naijab.sitnotify.newsmenu.model
 import com.google.gson.annotations.SerializedName
 
 data class NewsModel (
-        @SerializedName("_id")
-        val id:String,
-        val title:String,
-        val image:String,
-        val description:String,
-        val category:String,
-        val level:String,
-        val program:String,
-        val link:String,
-        val attached_file:String,
-        val is_important:String
+    @SerializedName("_id") val id:String,
+    @SerializedName("title") val title:String,
+    @SerializedName("image") val image:String,
+    @SerializedName("description") val description:String,
+    @SerializedName("category") val category:String,
+    @SerializedName("level") val level:String,
+    @SerializedName("program") val program:String,
+    @SerializedName("link") val link:String,
+    @SerializedName("attached_file") val attachedFile:String,
+    @SerializedName("created_date") val createdDate:String,
+    @SerializedName("edited_date") val editedDate:String,
+    @SerializedName("__v") val v:Int,
+    @SerializedName("is_important") val isImportant:Boolean
 )
+
