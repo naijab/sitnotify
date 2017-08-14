fc639e3a7ae72cbc92f16f04ef948fed627bb41b
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-14 19:32:07 +0700

    Add haddle response and error

--- app/src/main/java/com/naijab/sitnotify/newsmenu/NewsFragment.kt
+++ app/src/main/java/com/naijab/sitnotify/newsmenu/NewsFragment.kt
@@ -1,19 +1,25 @@
 package com.naijab.sitnotify.newsmenu
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.naijab.sitnotify.R
 import com.naijab.sitnotify.network.NotifyAPIConnect
+import com.naijab.sitnotify.newsmenu.adapter.NewsRecyclerViewAdapter
 import com.naijab.sitnotify.newsmenu.model.NewsModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragment : Fragment() {
+
+    private var mNewsArrayList: ArrayList<NewsModel>? = null
+    private var mAdapter: NewsRecyclerViewAdapter? = null
 
     companion object {
         fun newInstance(): NewsFragment {
@@ -28,19 +34,35 @@ class NewsFragment : Fragment() {
         return inflater!!.inflate(R.layout.fragment_news, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
+    }
+
+    fun setRecyclerView(){
+        newsRecycler.layoutManager = LinearLayoutManager(activity, LinearLayout.VERTICAL, false)
+    }
+
     fun getNews() {
         Log.e("News Fragment:", "Call")
         NotifyAPIConnect().loadNews()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { news ->
-                            Log.e("News Fragment:", news.get(0).title)
-                        },
-                        { error ->
-                            Log.e("News Fragment:", error.message)
-                        }
-                )
+                .subscribe(this::handleResponse,this::handleError)
+    }
+
+    fun handleResponse(newsList: List<NewsModel>) {
+        mNewsArrayList = ArrayList<NewsModel>(newsList)
+        setupRecyclerView(mNewsArrayList as ArrayList<NewsModel>)
+    }
+
+    fun handleError(error:Throwable){
+        Log.e("News Fragment",error.message)
+    }
+
+    fun setupRecyclerView(newsList: ArrayList<NewsModel>) {
+        mAdapter = NewsRecyclerViewAdapter(newsList)
+        newsRecycler.adapter = mAdapter
     }
 
 }
