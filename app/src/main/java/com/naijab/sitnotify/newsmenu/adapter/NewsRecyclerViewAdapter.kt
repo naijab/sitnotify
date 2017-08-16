094d1831d8f99ec4f9b23efb422dcae6481d53dd
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-16 12:47:51 +0700

    Add click listener for recycler view

--- app/src/main/java/com/naijab/sitnotify/newsmenu/adapter/NewsRecyclerViewAdapter.kt
+++ app/src/main/java/com/naijab/sitnotify/newsmenu/adapter/NewsRecyclerViewAdapter.kt
@@ -1,6 +1,5 @@
 package com.naijab.sitnotify.newsmenu.adapter
 
-import android.graphics.Bitmap
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,25 +13,33 @@ import com.naijab.sitnotify.newsmenu.model.NewsModel
 import java.util.ArrayList
 import kotlinx.android.synthetic.main.news_list_layout.view.*
 
-class NewsRecyclerViewAdapter(val newsList: ArrayList<NewsModel>) : RecyclerView.Adapter<NewsRecyclerViewAdapter.ViewHolder>() {
+class NewsRecyclerViewAdapter(val newsList: ArrayList<NewsModel>,
+                              val listener: (NewsModel) -> Unit)
+    : RecyclerView.Adapter<NewsRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.news_list_layout, parent, false)
-
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.bindItems(newsList[position])
+        val item = getItem(position)
+        holder?.bindItems(item, listener)
     }
 
     override fun getItemCount(): Int {
         return newsList.size
     }
 
+    private fun getItem(position: Int): NewsModel{
+        return newsList[position]
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindItems(news: NewsModel){
+
+        fun bindItems(news: NewsModel, listener: (NewsModel) -> Unit) = with(itemView){
             itemView.titleNews.text = news.title
+            setOnClickListener { listener(news) }
             Glide.with(itemView.context)
                     .load(news.image)
                     .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.RESOURCE))
