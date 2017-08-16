c27501f296f704ce59f7b2bf825b874125e63a3e
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-16 13:05:21 +0700

    Add newsDetail Activity

---
+++ app/src/main/java/com/naijab/sitnotify/newsmenu/detail/NewsDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.naijab.sitnotify.newsmenu.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.naijab.sitnotify.R
+
+class NewsDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+    }
+}
