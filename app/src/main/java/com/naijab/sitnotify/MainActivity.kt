92f166310d971f227ac255caf6d9a405aefedfd4
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-19 04:46:24 +0700

    Add call Service Fragment

--- app/src/main/java/com/naijab/sitnotify/MainActivity.kt
+++ app/src/main/java/com/naijab/sitnotify/MainActivity.kt
@@ -4,19 +4,15 @@ import android.os.Bundle
 import com.ashokvarma.bottomnavigation.BottomNavigationBar
 import com.ashokvarma.bottomnavigation.BottomNavigationItem
 import com.naijab.sitnotify.base.BaseActivity
-import com.naijab.sitnotify.newsmenu.ActivityFragment
-import com.naijab.sitnotify.newsmenu.CalendarFragment
-import com.naijab.sitnotify.newsmenu.MoreFragment
-import com.naijab.sitnotify.newsmenu.NewsFragment
+import com.naijab.sitnotify.newsmenu.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_activity_main.*
 
 class MainActivity : BaseActivity() {
 
     lateinit internal var fragmentNews: NewsFragment
-    lateinit internal var fragmentActivity: ActivityFragment
+    lateinit internal var fragmentService: ServiceFragment
     lateinit internal var fragmentCalendar: CalendarFragment
-    lateinit internal var fragmentMore: MoreFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +26,8 @@ class MainActivity : BaseActivity() {
 
     private fun setupFragment() {
         fragmentNews = NewsFragment.newInstance()
-        fragmentActivity = ActivityFragment.newInstance()
+        fragmentService = ServiceFragment.newInstance()
         fragmentCalendar = CalendarFragment.newInstance()
-        fragmentMore = MoreFragment.newInstance()
     }
 
     private fun initBottomNavBar() {
@@ -40,11 +35,9 @@ class MainActivity : BaseActivity() {
                 .addItem(BottomNavigationItem(
                         R.drawable.ic_news, getString(R.string.news_menu)))
                 .addItem(BottomNavigationItem(
-                        R.drawable.ic_activity, getString(R.string.activity_menu)))
-                .addItem(BottomNavigationItem(
                         R.drawable.ic_calendar, getString(R.string.calendar_menu)))
                 .addItem(BottomNavigationItem(
-                        R.drawable.ic_more, getString(R.string.more_menu)))
+                        R.drawable.ic_public, getString(R.string.service_menu)))
                 .initialise()
     }
 
@@ -66,9 +59,8 @@ class MainActivity : BaseActivity() {
         supportFragmentManager.beginTransaction().apply {
             when (position) {
                 0 -> replace(R.id.container, fragmentNews)
-                1 -> replace(R.id.container, fragmentActivity)
                 2 -> replace(R.id.container, fragmentCalendar)
-                3 -> replace(R.id.container, fragmentMore)
+                3 -> replace(R.id.container, fragmentService)
             }
         }.commitAllowingStateLoss()
     }
