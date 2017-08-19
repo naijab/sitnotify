5c9e8f4921a8c6985efaf5184912ff8ab88f61b7
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-08-19 13:23:22 +0700

    Add getService API

--- app/src/main/java/com/naijab/sitnotify/network/NotifyAPI.kt
+++ app/src/main/java/com/naijab/sitnotify/network/NotifyAPI.kt
@@ -2,6 +2,7 @@ package com.naijab.sitnotify.network
 
 import com.naijab.sitnotify.newsmenu.model.CalendarModel
 import com.naijab.sitnotify.newsmenu.model.NewsModel
+import com.naijab.sitnotify.servicemenu.model.ServiceModel
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -16,5 +17,9 @@ interface NotifyAPI {
     @GET("calendar")
     fun getCalendar(@Query("key") key: String)
     : Observable<List<CalendarModel>>
+
+    @GET("service")
+    fun getService(@Query("key") key: String)
+            : Observable<List<ServiceModel>>
 
 }
